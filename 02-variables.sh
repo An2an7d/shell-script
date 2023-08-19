162e0a2623b3eb7d1bc385fc47b6ad4a3d5f936d
Anand <venkatanandn@gmail.com>
2023-08-19 07:37:42 +0530

    new_changs

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+person1="ramesh"
+person2="suresh"
+
+echo "${person1}: Hi ${person2}"
+echo "${person2}: Hello ${person1}"
+echo "${person1}: Good Morning ${person2}!"
+echo "${person2}: Very Good Morning ${person1}!!!"
