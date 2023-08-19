865400dba6874c6cd11101e7e93ff539378f014e
Anand <venkatanandn@gmail.com>
2023-08-19 08:06:55 +0530

    new_changs

---
+++ 05-variables.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+DATE=$(date)
+echo "execution started at $(DATE)"
+
+echo -n "Enter Username:"
+read -s USERNAME
+
+echo
+
+echo -n "Enter password:"
+read -s PASSWORD
+
+echo
+
+echo "Username: ${USERNAME}"
+echo "password: ${PASSWORD}"
+
+echo "execution ended at $(DATE)"
