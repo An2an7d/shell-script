c9424385a69931cc451eae17394a0c140df65bc5
Anand <venkatanandn@gmail.com>
2023-08-22 20:16:52 +0530

    mail modified

--- mail.sh
+++ mail.sh
@@ -4,8 +4,8 @@ TO_ADDRESS=$1
 SUBJECT=$2
 BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
 echo "escaped content: $BODY"
-TEAM_NAME=$4
-ALERT_TYPE=$5
+TEAM_NAME=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $4)
+ALERT_TYPE=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $5)
 
 FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
 
