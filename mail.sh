c762221e02ede1f21db17b9d57876b480f1c5b6b
Anand <venkatanandn@gmail.com>
2023-08-22 19:58:37 +0530

    mail modified

--- mail.sh
+++ mail.sh
@@ -8,4 +8,4 @@ ALERTTYPE=$5
 
 FINAL_BODY=$(sed -e "s/TEAMNAME/$TEAMNAME/g" -e "s/ALERTTYPE/$ALERTTYPE/g" -e "s/BODY/$BODY/" template.html)
 
-echo "$FINAL_BODY" | mail -s $"(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
