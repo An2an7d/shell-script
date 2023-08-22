d9393888302fdf6be72adac1ba9db59ca2fe0809
Anand <venkatanandn@gmail.com>
2023-08-22 20:05:42 +0530

    mail modified

--- mail.sh
+++ mail.sh
@@ -3,9 +3,9 @@
 TO_ADDRESS=$1
 SUBJECT=$2
 BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
-TEAMNAME=$4
-ALERTTYPE=$5
+TEAM_NAME=$4
+ALERT_TYPE=$5
 
-FINAL_BODY=$(sed -e "s/TEAMNAME/$TEAMNAME/g" -e "s/ALERTTYPE/$ALERTTYPE/g" -e "s/BODY/$BODY/" template.html)
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/" template.html)
 
 echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
