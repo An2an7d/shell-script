3c67d0d8eee46031e40e069c27c4d382841587e2
Anand <venkatanandn@gmail.com>
2023-08-22 20:51:00 +0530

    disk_usage modified

--- mail.sh
+++ mail.sh
@@ -7,6 +7,6 @@ echo -e "escaped content: $BODY"
 TEAM_NAME=$(sed -e 's/[\/$*.^]/\\&/g' <<< $4)
 ALERT_TYPE=$(sed -e 's/[\/$*.^]/\\&/g' <<< $5)
 
-FINAL_BODY=$(sed -e "s/MESSAGE/$BODY/g" -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" template.html)
+FINAL_BODY=$(sed -e "s/MESSAGE/$3/g" -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" template.html)
 
 echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
