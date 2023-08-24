54255e8090d789c1e1afd87191f67da339b7e7b6
Anand <venkatanandn@gmail.com>
2023-08-24 18:25:17 +0530

    assignment2 added

--- 1_assignment.sh
+++ 1_assignment.sh
@@ -15,17 +15,16 @@ echo -e "$nline"
 
 #----------------------------------------
 nline=''
+mkdir test
 while read line
 do 
     for ((i=${#line}-1;i>=0;i--))
     do
         nline+=${line:$i:1}
     done
-    echo $nline >> $pwd/test/reverse.txt
+    echo $nline >> $PWD/test/reverse.txt
     nline=''
 done < file.txt
 
-# develop a script that takes a text file and replaces all occurences of 
-# a specific word or phrase with another word or phrase.
 # write a script that reads a text file and generates a list of words with
 # their frequencies. Bonus points for sorting the list by frequency.
---
+++ 2_assignment.sh
@@ -0,0 +1,7 @@
+# develop a script that takes a text file and replaces all occurences of 
+# a specific word or phrase with another word or phrase.
+
+word_to_find=line
+word_to_replace=test
+
+sed -i 's/$word_to_find/$word_to_replace/g file.txt'
