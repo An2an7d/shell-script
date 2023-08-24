6cc81de00abc26103a47d4809c50ccb7612d2f2b
Anand <venkatanandn@gmail.com>
2023-08-24 18:35:43 +0530

    assignment2 modified

--- 2_assignment.sh
+++ 2_assignment.sh
@@ -1,7 +1,8 @@
 # develop a script that takes a text file and replaces all occurences of 
 # a specific word or phrase with another word or phrase.
+#!/bin/bash
 
 word_to_find=line
 word_to_replace=test
 
-sed -i 's/$word_to_find/$word_to_replace/g file.txt'
+sed -i "s/$word_to_find/$word_to_replace/g" file.txt
